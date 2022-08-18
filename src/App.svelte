5ffd230833c78f5c44d65acbaeee8bd5f9d9c96e
Mathieu Andrade <mathieu.botelhoandrade@gmail.com>
2022-08-18 22:32:30 +0200

    Add confirmation before closing tab when data is pending save

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,20 @@
   import Mobile from './views/Mobile.svelte';
   import Logs from './views/Logs.svelte';
 
+  import { hasDataToSave, addToast } from './utils/store';
+  import _ from './utils/i18n';
+
+  const target = import.meta.env.VITE_TARGET;
+
+  // Prompt to save data before closing
+  window.onbeforeunload = () => {
+    console.log(target);
+    if ($hasDataToSave) {
+      addToast('warning', $_('dataWaitingToBeSaved'));
+      return target === 'soft' ? undefined : 'Do you really want to close?';
+    }
+    return undefined;
+  };
 
   const routes = {
     '/': Home,
